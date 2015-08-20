66f22f6f3829e7ba403fef4227a2f533d8af0567
BeiDream <331341164@qq.com>
2015-08-20 23:07:11 +0800

    租户应用程序管理

--- Presentation/01-Applications.Presentation/Areas/Systems/Controllers/ApplicationController.cs
+++ Presentation/01-Applications.Presentation/Areas/Systems/Controllers/ApplicationController.cs
@@ -1,7 +1,11 @@
-﻿using Applications.Domains.Queries.Systems;
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using Applications.Domains.Queries.Systems;
 using Applications.Services.Contracts.Systems;
 using Applications.Services.Dtos.Systems;
 using Presentation.Base;
+using Util.Webs.Mvc;
 
 namespace Presentation.Areas.Systems.Controllers {
     /// <summary>
@@ -14,6 +18,20 @@ namespace Presentation.Areas.Systems.Controllers {
         /// <param name="applicationService">应用程序服务</param>
         public ApplicationController( IApplicationService applicationService )
             : base( applicationService ) {
+        }
+
+        /// <summary>
+        /// 查询
+        /// </summary>
+        /// <param name="query"></param>
+        /// <param name="tenantId">查询对象</param>
+        [HttpPost]
+        [AjaxOnly]
+        public ActionResult Query(ApplicationQuery query, Guid tenantId)
+        {
+            SetPage(query);
+            var result = Service.Query(query);
+            return ToDataGridResult(ConvertQueryResult(result).ToList(), result.TotalCount);
         }
     }
 }
